af782dc0d7ab6e0ffcf39325332033f3fa80714a
AlitaChu <scriptales.clients@gmail.com>
2020-02-26 11:29:42 +0100

    Affichage modale

--- script_entrees.js
+++ script_entrees.js
@@ -4,11 +4,13 @@ console.log('OK');
 var recettes = [
     {
         titre: "Salade Niçoise", 
-        page: "recette_salade_nicoise.html"
+        page: "recette_salade_nicoise.html",
+        image: "salade.jpg"
     },
     {
         titre: "Tarte aux poireaux", 
-        page: "recette_tarte_poireaux.html"
+        page: "recette_tarte_poireaux.html",
+        image: "tarte.jpg"
     }
 ];
 
@@ -20,8 +22,31 @@ for(var i = 0; i < len; i++) {
     // Accés recette => recettes[i]
     // Accés titre d'une recette => recettes[i].titre
     // <li><a href="pages_entrees/recette_salade_nicoise.html">Salade Niçoise</a></li>
-   html += "<li><a href=\"pages_entrees/" + recettes[i].page + "\">" + recettes[i].titre + "</a></li>";
+   html += "<li class=\"elem\" id=\"rec" + i + "\">" + recettes[i].titre + "</a></li>";
 }
 
 var liste = document.getElementById("liste");
 liste.innerHTML = html;
+
+
+/* Fonctionnalité modale */
+
+var container = document.getElementById("rec_container");
+container.addEventListener("click", () => {
+    container.style.display = "none";
+});
+
+var elems = document.getElementsByClassName("elem");
+var len = elems.length;
+for(var i = 0; i < len; i++) {
+    elems[i].addEventListener("click", (event) => {
+        event.preventDefault();
+        var index = parseInt(event.target.id.substring(3));
+        showRecette(index);
+    });
+}
+
+function showRecette(index) {
+    console.log("Vous avez demandé la recette " + recettes[index].titre);
+    container.style.display = "block";
+}
